68e3fbfc210623cbca2098a1b10585fbeda969f1
Heeyoung Jung <appcreator05@AppCreator05ui-iMac.local>
2016-09-29 19:22:31 +0900

    #3 Edit Mainactivity and Layout

--- app/src/main/java/kr/hs/emirim/young24/touchgraphic/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/young24/touchgraphic/MainActivity.java
@@ -13,7 +13,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new DrawShape(getApplicationContext()));
+        setContentView(R.layout.activity_main);
+    }
+
+    public void drawShape(View v){
+
     }
     class DrawShape extends View {
         DrawShape(Context context){ //상속을 받았기에 그에 맞는 부모 객체를 만들어줘야함
