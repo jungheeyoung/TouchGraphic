d693a978929b435166766bd6dbb17b154fff576b
Heeyoung Jung <appcreator05@AppCreator05ui-iMac.local>
2016-09-29 20:02:27 +0900

    #4 add handler method

--- app/src/main/java/kr/hs/emirim/young24/touchgraphic/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/young24/touchgraphic/MainActivity.java
@@ -6,18 +6,32 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final int LINE = 1, RECT = 2, CIRCLE = 3;
+    DrawShape ds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ds = new DrawShape(getApplicationContext());
+        LinearLayout linear = (LinearLayout) findViewById(R.id.linear_view);
+        linear.addView(ds);
+
     }
 
     public void drawShape(View v){
-
+        switch(v.getId()){
+            case R.id.but_line:
+                break;
+            case R.id.but_rect:
+                break;
+            case R.id.but_circle:
+                break;
+        }
     }
     class DrawShape extends View {
         DrawShape(Context context){ //상속을 받았기에 그에 맞는 부모 객체를 만들어줘야함
